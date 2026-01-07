c15ce9b863c91f4e1e71fb68d553f5467e77c9e9
RegistryHJ <registry.hj@outlook.com>
2026-01-08 04:58:49 +0900

    fix: Fix RouterProvider routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,25 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
-import './main.css';
-import IndexPage from './pages';
-import MenuPage from './pages/menu';
-import OrdersPage from './pages/orders';
-import OrderDetailsPage from './pages/orders/[tableId]';
-import CartPage from './pages/cart/[tableId]';
+import "@/main.css";
+import IndexPage from "@/pages";
+import MenuPage from "@/pages/menu";
+import OrdersPage from "@/pages/orders";
+import OrderDetailsPage from "@/pages/order-details";
+import CartPage from "@/pages/cart";
+import SettingsPage from "@/pages/settings";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider
       router={createBrowserRouter([
-        { path: '/', element: <IndexPage /> },
-        { path: '/orders', element: <OrdersPage /> },
-        { path: '/orders/:tableId', element: <OrderDetailsPage /> },
-        { path: '/menu', element: <MenuPage /> },
-        { path: '/cart/:tableId', element: <CartPage /> },
+        { path: "/", element: <IndexPage /> },
+        { path: "/orders", element: <OrdersPage /> },
+        { path: "/order-details", element: <OrderDetailsPage /> },
+        { path: "/cart", element: <CartPage /> },
+        { path: "/menu", element: <MenuPage /> },
+        { path: "/settings", element: <SettingsPage /> },
       ])}
     />
   </StrictMode>,
