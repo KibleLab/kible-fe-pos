8d8fea58882a83b6e2f9b5f03c9447a92d230e77
RegistryHJ <registry.hj@outlook.com>
2026-01-14 04:18:32 +0900

    feat: Add DataGrid and Function Buttons to OrderDetailsPage

--- src/pages/order-details/index.tsx
+++ src/pages/order-details/index.tsx
@@ -1,4 +1,7 @@
+import { useState, type CSSProperties } from "react";
 import { useNavigate, useSearchParams } from "react-router";
+import { type ColDef } from "ag-grid-community";
+import { AgGridReact } from "ag-grid-react";
 import { cn, formatOrderType } from "@/lib/utils";
 import AppBar from "@/components/AppBar";
 import AppBarTextButton from "@/components/AppBar/AppBarTextButton";
@@ -6,10 +9,18 @@ import AppBarIconButton from "@/components/AppBar/AppBarIconButton";
 import ContainedButton from "@/components/Common/ContainedButton";
 import DataLabel from "@/components/Common/DataLabel";
 import TitleLabel from "@/components/Common/TitleLabel";
+import OutlinedButton from "@/components/Common/OutlinedButton";
 
 export default function OrderDetailsPage() {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
+  const [columnDefs] = useState<ColDef[]>([
+    { headerName: "ID", field: "id", flex: 0.5 },
+    { headerName: "상품명", field: "menu_name", flex: 2 },
+    { headerName: "단가", field: "menu_price", flex: 1 },
+    { headerName: "수량", field: "order_quantity", flex: 1 },
+    { headerName: "상태", field: "order_status", flex: 1 },
+  ]);
   const orderType = searchParams.get("orderType");
   const orderNo = searchParams.get("orderNo");
   return (
@@ -31,7 +42,7 @@ export default function OrderDetailsPage() {
       <main>
         <div
           className={cn(
-            "flex flex-wrap gap-9",
+            "flex flex-col flex-wrap gap-9",
             "fixed top-25.5 left-0",
             "w-315 h-244.5 p-9",
             "overflow-y-auto",
@@ -42,8 +53,40 @@ export default function OrderDetailsPage() {
             </TitleLabel>
             {/* Reservation Area */}
           </div>
-          <div className={cn("flex flex-wrap w-full h-202")}>
-            {/* DataGrid Area */}
+          <div
+            className={cn("flex flex-wrap w-full h-170.5")}
+            style={{ "--ag-font-size": "20px" } as CSSProperties}>
+            <AgGridReact
+              className={cn("ag-theme-quartz", "w-full h-full")}
+              defaultColDef={{ resizable: false, suppressMovable: true }}
+              rowSelection={{ mode: "multiRow", headerCheckbox: true }}
+              rowData={[]}
+              columnDefs={columnDefs}
+            />
+          </div>
+          <div
+            className={cn(
+              "flex flex-row justify-between, gap-9",
+              "w-full h-22.5",
+            )}>
+            <OutlinedButton className={cn("w-full h-full")}>
+              전체 취소
+            </OutlinedButton>
+            <OutlinedButton className={cn("w-full h-full")}>
+              선택 취소
+            </OutlinedButton>
+            <OutlinedButton className={cn("w-full h-full")}>
+              수량 변경
+            </OutlinedButton>
+            <OutlinedButton className={cn("w-full h-full")}>
+              할인 적용
+            </OutlinedButton>
+            <OutlinedButton className={cn("w-full h-full")}>
+              접수로 변경
+            </OutlinedButton>
+            <OutlinedButton className={cn("w-full h-full")}>
+              완료로 변경
+            </OutlinedButton>
           </div>
         </div>
         <div
